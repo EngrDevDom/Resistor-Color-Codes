ea2e2b54b8d2d7bbdee9c43f3d6f833d119bb7b2
Krys <jackpschmid@gmail.com>
2017-01-11 17:23:50 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 def decode_resistor_colors(bands):
-    res = {'black' : 0, 'brown' : 1, 'red' : 2, 'orange' : 3, 'yellow' : 4, 'green' : 5, 'blue' : 6, 'violet' : 7, 'gray' : 8, 'white' : 9, 'gold' : 5, 'silver' : 10}
+    res = {'black' : '0', 'brown' : '1', 'red' : '2', 'orange' : '3', 'yellow' : '4', 'green' : '5', 'blue' : '6', 'violet' : '7', 'gray' : '8', 'white' : '9', 'gold' : '5', 'silver' : '10'}
     code = bands.split(" ")
-    power = int(res[code[0]] + res[code[1]]) * int(res[code[2]])
+    print(code)
+    print(int(res[code[0]] + res[code[1]]) *(10 ** int(res[code[2]])))
+    power = int(res[code[0]] + res[code[1]]) *(10 ** int(res[code[2]]))
     if power < 1000:
-        output = power + ' ohms, '
-    elif power < 100000:
-         output = (power / 1000) + 'k ohms, '
+        output = str(power) + ' ohms, '
+    elif power < 1000000:
+        if int(power / 1000) == float(power / 1000):
+             output = str(int(power / 1000)) + 'k ohms, '
+        else:
+             output = str(float(power / 1000000)) + 'M ohms, '
     else:
-         output = (power / 1000000) + 'M ohms, '
-    if len(code) is 3:
-        output.join('20%')
+        if int(power / 1000000) == float(power / 1000000):
+             output = str(int(power / 1000000)) + 'M ohms, '
+        else:
+             output = str(float(power / 1000000)) + 'M ohms, '
+         
+    if len(code) == 3:
+        output += ('20%')
     else:
-        output.join(res[code[3]] + '%')
+        output += (res[code[3]] + '%')
     return output
